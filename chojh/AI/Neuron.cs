782cda090972835f5eec5433655ee8be0524d700
ChoJungHee <junghee@zete.co.kr>
2022-06-05 12:27:57 +0900

    Add AI Framework

---
+++ chojh/AI/Neuron.cs
@@ -0,0 +1,179 @@
+﻿using System;
+
+public class Neuron{
+  public int Input_Length = 16;
+  public double Result;
+  private string Active_Function_Name;
+  public double[] Input_Signal;
+  public double[] Weight;
+  private double[] Update_Weight;
+  private double[] Moment_Term;
+  private double Bias_Input;
+  public static double Threhold = 0.5;
+  public static double Learning_Rate = 0.001;
+  public static double Beta = 0.0005;
+  public double Error;
+
+  public void Set_Learning(double a, double b){
+    Neuron.Learning_Rate = a;
+    Neuron.Beta = b;
+  }
+
+  public Neuron(int Input_Length){
+    this.Input_Length = Input_Length;
+    this.Input_Signal = new double[Input_Length];
+    this.Weight = new double[checked (Input_Length + 1)];
+    this.Update_Weight = new double[checked (Input_Length + 1)];
+    this.Moment_Term = new double[checked (Input_Length + 1)];
+    this.Init();
+  }
+
+  public Neuron(int Input_Length, string Active_Function_Name){
+    this.Input_Length = Input_Length;
+    this.Input_Signal = new double[Input_Length];
+    this.Weight = new double[checked (Input_Length + 1)];
+    this.Update_Weight = new double[checked (Input_Length + 1)];
+    this.Active_Function_Name = Active_Function_Name;
+    this.Moment_Term = new double[checked (Input_Length + 1)];
+    this.Init();
+  }
+
+  public void Set_Input_Length(int Length){
+    this.Input_Length = Length;
+    this.Input_Signal = new double[Length];
+    this.Weight = new double[checked (Length + 1)];
+    this.Update_Weight = new double[checked (this.Input_Length + 1)];
+    this.Moment_Term = new double[checked (this.Input_Length + 1)];
+    this.Init();
+  }
+
+  public void Init(){
+    double num = 1.0 / Math.Sqrt((double) this.Input_Length);
+    double MAX = num;
+    this.Weight = Util.NotOverRandomArray(checked (this.Input_Length + 1), -num, MAX);
+    int index = 0;
+    while (index < this.Moment_Term.Length){
+      this.Moment_Term[index] = 0.0;
+      checked { ++index; }
+    }
+    this.Bias_Input = 1.0;
+  }
+
+  public double Propagate(double[] Signal){
+    double num = 0.0;
+    int index = 0;
+    while (index < Signal.Length){
+      num += Signal[index] * this.Weight[index];
+      this.Input_Signal[index] = Signal[index];
+      checked { ++index; }
+    }
+    double X = num + this.Bias_Input * this.Weight[this.Input_Length];
+    switch (this.Active_Function_Name){
+      case "TanH":
+        this.Result = this.TanH(X);
+        break;
+      case "Sigmoid":
+        this.Result = this.Sigmoid(X);
+        break;
+      case "ReLU":
+        this.Result = this.ReLU(X);
+        break;
+      case "Linear":
+        this.Result = this.Linear(X);
+        break;
+      default:
+        this.Result = this.TanH(X);
+        break;
+    }
+    return this.Result;
+  }
+
+  public void Propagate_Thread() => this.Result = this.Propagate(this.Input_Signal);
+
+  public void Update_Commit(){
+    int index = 0;
+    while (index < this.Input_Length)
+    {
+      this.Weight[index] = this.Update_Weight[index];
+      checked { ++index; }
+    }
+    this.Weight[this.Input_Length] = this.Update_Weight[this.Input_Length];
+  }
+
+  public void BP_Update(double Error_Delta){
+    double num;
+    switch (this.Active_Function_Name){
+      case "TanH":
+        num = 1.0 - Math.Pow(this.Result, 2.0);
+        break;
+      case "Sigmoid":
+        num = this.Result * (1.0 - this.Result);
+        break;
+      case "ReLU":
+        num = 1.0;
+        break;
+      case "Linear":
+        num = 1.0;
+        break;
+      default:
+        num = 1.0 - Math.Pow(this.Result, 2.0);
+        break;
+    }
+    int index = 0;
+    while (index < this.Input_Length){
+      this.Weight[index] = this.Weight[index] + Neuron.Learning_Rate * Error_Delta * this.Input_Signal[index] * num;
+      checked { ++index; }
+    }
+    this.Weight[this.Input_Length] = this.Weight[this.Input_Length] + Neuron.Learning_Rate * Error_Delta * this.Bias_Input * num;
+    this.Error = Error_Delta;
+  }
+
+  public double Distance_Function(double[] Signal){
+    double num = 0.0;
+    int index = 0;
+    while (index < Signal.Length){
+      num += this.Weight[index] - Signal[index];
+      checked { ++index; }
+    }
+    return num + (this.Weight[this.Input_Length] - 1.0);
+  }
+
+  public void Self_Organizing(double[] Signal){
+    this.Propagate(Signal);
+    double num = 1.0;
+    int index = 0;
+    while (index < Signal.Length){
+      this.Weight[index] = this.Weight[index] + Neuron.Beta * (Signal[index] - this.Weight[index]) * 0.5 * num;
+      checked { ++index; }
+    }
+    this.Weight[this.Input_Length] = this.Weight[this.Input_Length] + Neuron.Learning_Rate * (1.0 - this.Weight[this.Input_Length]) * num;
+  }
+
+  public void Hebb_Rule(double[] Signal){
+    double num1 = this.Propagate(Signal);
+    double num2 = 1.0;
+    int index = 0;
+    while (index < Signal.Length){
+      this.Weight[index] = this.Weight[index] + Neuron.Learning_Rate * num1 * (Neuron.Beta * Signal[index] - this.Weight[index]) * num2;
+      checked { ++index; }
+    }
+    this.Weight[this.Input_Length] = this.Weight[this.Input_Length] + Neuron.Learning_Rate * num1 * (Neuron.Beta * this.Bias_Input - this.Weight[this.Input_Length]) * num2;
+  }
+
+  private double TanH(double X) => Math.Tanh(X);
+
+  private double Sigmoid(double X) => 1.0 / (1.0 + Math.Exp(-X));
+
+  private double ReLU(double X) => X > 0.0 ? X : 0.0;
+
+  private double Linear(double X) => X;
+
+  public void Show_Weight(){
+    int index = 0;
+    while (index < this.Weight.Length){
+      Console.Write(Math.Round(this.Weight[index], 6).ToString() + " ");
+      checked { ++index; }
+    }
+    Console.WriteLine();
+  }
+}
---
+++ chojh/AI/Neuron_Layer.cs
@@ -0,0 +1,240 @@
+﻿using System;
+
+public class Neuron_Layer{
+  public Neuron[] N;
+  public static double Alpha = 0.15;
+  public double[] Result;
+  public int Neuron_Length;
+  public int Input_Length;
+  private string Active_Function_List;
+  private int Update_Radius;
+  private int Enbale_Neuron;
+
+  public void Set_Learning(double a, double b){
+    int index = 0;
+    while (index < this.N.Length){
+      this.N[index].Set_Learning(a, b);
+      checked { ++index; }
+    }
+  }
+
+  public Neuron_Layer(int Input_Length, int Neuron_Length){
+    this.Input_Length = Input_Length;
+    this.Neuron_Length = Neuron_Length;
+    this.Enbale_Neuron = checked ((int) unchecked ((double) Neuron_Length * Neuron_Layer.Alpha));
+    if (this.Enbale_Neuron < 10)
+      this.Enbale_Neuron = Neuron_Length;
+    this.Update_Radius = checked ((int) unchecked ((double) this.Enbale_Neuron * 0.5));
+    if (this.Update_Radius < 1)
+      this.Update_Radius = 1;
+    this.Result = new double[Neuron_Length];
+    this.N = new Neuron[Neuron_Length];
+    int index = 0;
+    while (index < Neuron_Length){
+      this.N[index] = new Neuron(Input_Length);
+      checked { ++index; }
+    }
+    Console.WriteLine("뉴런 길이=" + Neuron_Length.ToString() + "  활성반경=" + this.Update_Radius.ToString());
+  }
+
+  public Neuron_Layer(int Input_Length, int Neuron_Length, string Active_Function_Name){
+    this.Input_Length = Input_Length;
+    this.Neuron_Length = Neuron_Length;
+    this.Active_Function_List = Active_Function_Name;
+    this.Enbale_Neuron = checked ((int) unchecked ((double) Neuron_Length * Neuron_Layer.Alpha));
+    if (this.Enbale_Neuron < 10)
+      this.Enbale_Neuron = Neuron_Length;
+    this.Update_Radius = checked ((int) unchecked ((double) this.Enbale_Neuron * 0.5));
+    if (this.Update_Radius < 1)
+      this.Update_Radius = 1;
+    this.Result = new double[Neuron_Length];
+    this.N = new Neuron[Neuron_Length];
+    int index = 0;
+    while (index < Neuron_Length){
+      this.N[index] = new Neuron(Input_Length, Active_Function_Name);
+      checked { ++index; }
+    }
+    Console.WriteLine("뉴런 길이=" + Neuron_Length.ToString() + "  활성반경=" + this.Update_Radius.ToString() + " 활성함수= " + Active_Function_Name);
+  }
+
+  public void Neuron_Input_Length_Set(int Length){
+    int index = 0;
+    while (index < this.N.Length){
+      this.N[index].Set_Input_Length(Length);
+      checked { ++index; }
+    }
+  }
+
+  public void Init(){
+    int index = 0;
+    while (index < this.N.Length){
+      this.N[index].Init();
+      checked { ++index; }
+    }
+  }
+
+  public double[] Propagate_SingleThreading(double[] Signal){
+    int index1 = 0;
+    while (index1 < this.Neuron_Length){
+      this.Result[index1] = this.N[index1].Propagate(Signal);
+      checked { ++index1; }
+    }
+    switch (this.Active_Function_List){
+      case "Softmax":
+        double num1 = 0.0;
+        int index2 = 0;
+        while (index2 < this.Neuron_Length){
+          num1 += Math.Exp(this.Result[index2]);
+          checked { ++index2; }
+        }
+        int index3 = 0;
+        while (index3 < this.Neuron_Length){
+          this.Result[index3] = Math.Exp(this.Result[index3]) / num1;
+          checked { ++index3; }
+        }
+        break;
+      case "Max":
+        double num2 = this.Result[0];
+        int index4 = 0;
+        int index5 = 0;
+        while (index5 < this.Neuron_Length){
+          if (num2 < this.Result[index5]){
+            num2 = this.Result[index5];
+            index4 = index5;
+          }
+          this.Result[index5] = 0.0;
+          checked { ++index5; }
+        }
+        this.Result[index4] = 1.0;
+        break;
+    }
+    return this.Result;
+  }
+
+  public void Update_Commit(){
+    int index = 0;
+    while (index < this.N.Length){
+      this.N[index].Update_Commit();
+      checked { ++index; }
+    }
+  }
+
+  public void BP_Update(double[] Error){
+    int index = 0;
+    while (index < this.N.Length){
+      this.N[index].BP_Update(Error[index]);
+      checked { ++index; }
+    }
+  }
+
+  public void BP_Update(double Error_Delta){
+    int index = 0;
+    while (index < this.Neuron_Length){
+      this.N[index].BP_Update(Error_Delta);
+      checked { ++index; }
+    }
+  }
+
+  public void Self_Organizing(double[] Signal){
+    double num1 = this.N[0].Distance_Function(Signal);
+    int index1 = 0;
+    int index2 = 0;
+    while (index2 < this.N.Length){
+      double num2 = this.N[index2].Distance_Function(Signal);
+      if (num1 < num2){
+        num1 = num2;
+        index1 = index2;
+      }
+      checked { ++index2; }
+    }
+    this.N[index1].Self_Organizing(Signal);
+    if (checked (index1 - this.Update_Radius) > 0){
+      int index3 = checked (index1 - this.Update_Radius);
+      while (index3 < index1){
+        this.N[index3].Self_Organizing(Signal);
+        checked { ++index3; }
+      }
+    }
+    else{
+      int index4 = 0;
+      while (index4 < index1){
+        this.N[index4].Self_Organizing(Signal);
+        checked { ++index4; }
+      }
+    }
+    if (checked (index1 + this.Update_Radius) < this.Neuron_Length){
+      int index5 = checked (index1 + 1);
+      while (index5 < checked (index1 + this.Update_Radius)){
+        this.N[index5].Self_Organizing(Signal);
+        checked { ++index5; }
+      }
+    }
+    else{
+      int index6 = checked (index1 + 1);
+      while (index6 < this.Neuron_Length){
+        this.N[index6].Self_Organizing(Signal);
+        checked { ++index6; }
+      }
+    }
+  }
+
+  public void Hebb_With_SOM(double[] Signal){
+    double num1 = this.N[0].Distance_Function(Signal);
+    int index1 = 0;
+    int index2 = 0;
+    while (index2 < this.N.Length){
+      double num2 = this.N[index2].Distance_Function(Signal);
+      if (num1 < num2){
+        num1 = num2;
+        index1 = index2;
+      }
+      checked { ++index2; }
+    }
+    this.N[index1].Hebb_Rule(Signal);
+    if (checked (index1 - this.Update_Radius) > 0){
+      int index3 = checked (index1 - this.Update_Radius);
+      while (index3 < index1){
+        this.N[index3].Hebb_Rule(Signal);
+        checked { ++index3; }
+      }
+    }
+    else{
+      int index4 = 0;
+      while (index4 < index1){
+        this.N[index4].Hebb_Rule(Signal);
+        checked { ++index4; }
+      }
+    }
+    if (checked (index1 + this.Update_Radius) < this.Neuron_Length){
+      int index5 = checked (index1 + 1);
+      while (index5 < checked (index1 + this.Update_Radius)){
+        this.N[index5].Hebb_Rule(Signal);
+        checked { ++index5; }
+      }
+    }
+    else{
+      int index6 = checked (index1 + 1);
+      while (index6 < this.Neuron_Length){
+        this.N[index6].Hebb_Rule(Signal);
+        checked { ++index6; }
+      }
+    }
+  }
+
+  public void Hebb_Rule(double[] Signal){
+    int index = 0;
+    while (index < this.N.Length){
+      this.N[index].Hebb_Rule(Signal);
+      checked { ++index; }
+    }
+  }
+
+  public void Show_Weight(){
+    int index = 0;
+    while (index < this.Neuron_Length){
+      this.N[index].Show_Weight();
+      checked { ++index; }
+    }
+    Console.WriteLine();
+  }
+}
